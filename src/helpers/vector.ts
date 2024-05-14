da6cdc3f4d2d4e473259cdf72387739e3baa9034
Mike Weaver <michael@triplewhale.com>
2024-05-14 14:06:59 -0400

    Try to filter data better

--- src/helpers/vector.ts
+++ src/helpers/vector.ts
@@ -6,36 +6,44 @@ import { sitemapUrl } from './constants'
 
 let docs: any
 export const vector = async (question: string) => {
+  loggy(`[vector] searching "${question}"`)
+
   if (!docs || docs.length === 0 || !Array.isArray(docs)) {
     const loader = new SitemapLoader(sitemapUrl, {
       selector: '.article-content', //extract article content only,
     })
 
     docs = await loader.load()
-    loggy(`[knowledge_base] loaded sitemap`)
+    loggy(`[vector] loaded sitemap`)
   }
 
   if (Array.isArray(docs) && docs.length > 0) {
-    const qArray = question.split(' ').filter((v) => v.length > 2)
-
-    const d = docs
+    let d = docs
       .filter((d: any) => d.pageContent && d.metadata)
-      .filter((d: any) =>
-        qArray.some(
-          (v) => d.metadata.title?.indexOf(v) >= 1 || d.metadata.description?.indexOf(v) >= 1,
-        ),
-      )
-      .slice(0, 25)
+      .filter((d: any) => JSON.stringify(d)?.indexOf(question) >= 1)
+      .slice(0, 5)
+
+    if (d.length === 0) {
+      const qArray = question.split(' ').filter((v) => v.length > 2)
+      d = docs
+        .filter((d: any) => d.pageContent && d.metadata)
+        .filter((d: any) =>
+          qArray.some(
+            (v) => d.metadata.title?.indexOf(v) >= 1 || d.metadata.description?.indexOf(v) >= 1,
+          ),
+        )
+        .slice(0, 5)
+    }
 
     const hnsw = await HNSWLib.fromDocuments(d, new OpenAIEmbeddings())
-    loggy(`[knowledge_base] fed vector store`)
+    loggy(`[vector] fed vector store`)
 
     const results = await hnsw.similaritySearch(question)
-    loggy(`[knowledge_base] queried the vector store`)
+    loggy(`[vector] queried the vector store`)
 
     return results
   }
 
-  loggy(`[knowledge_base] no documents found`)
+  loggy(`[vector] no documents found`)
   return []
 }
