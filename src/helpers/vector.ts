fff91e4ca6189d4257dc9f9b49a1e71fd5f0eb9d
Mike Weaver <michael@triplewhale.com>
2024-05-14 13:53:56 -0400

    Better vector data filtering & handling

--- src/helpers/vector.ts
+++ src/helpers/vector.ts
@@ -5,8 +5,6 @@ import { SitemapLoader } from 'langchain/document_loaders/web/sitemap'
 import { sitemapUrl } from './constants'
 
 let docs: any
-let hnsw: any
-
 export const vector = async (question: string) => {
   if (!docs || docs.length === 0 || !Array.isArray(docs)) {
     const loader = new SitemapLoader(sitemapUrl, {
@@ -22,16 +20,17 @@ export const vector = async (question: string) => {
 
     const d = docs
       .filter((d: any) => d.pageContent && d.metadata)
-      .filter((d: any) => qArray.some((v) => JSON.stringify(d).indexOf(v) >= 1))
-      .slice(0, 50)
+      .filter((d: any) =>
+        qArray.some(
+          (v) => d.metadata.title?.indexOf(v) >= 1 || d.metadata.description?.indexOf(v) >= 1,
+        ),
+      )
+      .slice(0, 25)
 
-    if (!hnsw) {
-      hnsw = await HNSWLib.fromDocuments(d, new OpenAIEmbeddings())
-      loggy(`[knowledge_base] fed vector store`)
-    }
+    const hnsw = await HNSWLib.fromDocuments(d, new OpenAIEmbeddings())
+    loggy(`[knowledge_base] fed vector store`)
 
-    const retriever = hnsw.asRetriever()
-    const results = await retriever.getRelevantDocuments(question)
+    const results = await hnsw.similaritySearch(question)
     loggy(`[knowledge_base] queried the vector store`)
 
     return results
